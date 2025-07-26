a0f54b1e1342b4b52c4352f7690f2d23f0512d9a
arseni_Ub <novakarsenya@gmail.com>
2025-07-26 08:55:41 +0300

    create: .env

--- project_rs/project_rs/settings.py
+++ project_rs/project_rs/settings.py
@@ -9,24 +9,25 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+DEFAULT_SECRET_KEY = "django-insecure-z@(1ylv"
+SECRET_KEY = os.getenv("SECRET_KEY", default=DEFAULT_SECRET_KEY)
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ey6ezv_p@($7-fbxv(--!*lswmc%pta9+yt)1^6o4m6xkp(p#$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", default=True)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
